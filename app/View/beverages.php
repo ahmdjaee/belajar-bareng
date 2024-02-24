6becbfb82722be4fc0cd4648844029bdc856c9f2
Ihsan Alif hidayah <ihsanalif10@gmail.com>
2024-02-24 17:32:32 +0700

    add beverages & css

---
+++ app/View/beverages.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beverages List</title>
+    <link rel="stylesheet" href="Style/beverages.css">
+</head>
+<body>
+        <h2>List Minuman</h2>
+        <div class="minuman">
+            <?php for ($i=0; $i <=10; $i++) : ?>
+                <div class="list">
+                    <img src="https://p-id.ipricegroup.com/media/Indira/minuman_keras.png" alt="Minuman keras">
+                    <div class="container">
+                    <h4>Rp. 60.000</h4>
+                    <h4>Anggur Merah</h4>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio nulla non suscipit esse culpa incidunt. Iusto libero enim porro repellendus est ex dicta non numquam? Laborum omnis fuga debitis a.</p>
+                </div>
+                <?php endfor?>
+        </div>
+    </body>
+</html>

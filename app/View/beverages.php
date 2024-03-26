ec8ce53b1b1912601f8b7beb6744914c19e0ab25
nurcahyadi <nurcahyadiw162599@gmail.com>
2024-03-26 21:23:59 +0700

    perubahan beverages.php

--- app/View/beverages.php
+++ app/View/beverages.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beverages List</title>
     <link rel="stylesheet" href="Style/beverages.css">
 </head>
+
 <body>
-        <h2>List Minuman</h2>
-        <div class="minuman">
-            <?php for ($i=0; $i <=10; $i++) : ?>
-                <div class="list">
-                    <img src="https://p-id.ipricegroup.com/media/Indira/minuman_keras.png" alt="Minuman keras">
-                    <div class="container">
-                    <h4>Rp. 60.000</h4>
-                    <h4>Anggur Merah</h4>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio nulla non suscipit esse culpa incidunt. Iusto libero enim porro repellendus est ex dicta non numquam? Laborum omnis fuga debitis a.</p>
-                </div>
-                <?php endfor?>
+    <!-- <h2>List Minuman</h2> -->
+
+    <div class="minuman">
+
+        <?php for ($i = 0; $i < 10; $i++): ?>   
+            <div class="container">
+                <img src="https://p-id.ipricegroup.com/media/Indira/minuman_keras.png" alt="Minuman keras" width="200px">
+                <h4>Rp. 60.000</h4>
+                <h4>Anggur Merah</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio nulla non suscipit esse culpa incidunt.
+                    Iusto libero enim porro repellendus est ex dicta non numquam? Laborum omnis fuga debitis a.</p>
+            
         </div>
-    </body>
+        <?php endfor ?>
+</body>
+
 </html>
